1058be03c03bc87cfaebe344a253a3fe24246725
Qais Aziz <azizqais96@gmail.com>
2022-08-29 12:40:28 +0500

    1st Commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI,
+     {useNewUrlParser: true, useUnifiedTopology: true}).then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err.message}`);
+    })
 
 
 
