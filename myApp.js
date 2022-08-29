ab2e215c16d796cc7598f621cd11e3dff2435e8c
Qais Aziz <azizqais96@gmail.com>
2022-08-29 10:31:57 +0500

    1st Commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
-
+let mongoos = require('mongoose');
+mongoose.connect('mongodb+srv://qais_aziz:fh4uZkfZW8EycgB@cluster0.3zpuzxk.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 
